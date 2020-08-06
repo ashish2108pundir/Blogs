d31dc90d269727269b27ed1ef08762e41f8fb221
ashish2108pundir <64045031+ashish2108pundir@users.noreply.github.com>
2020-08-07 00:40:15 +0530

    Add files via upload

---
+++ Blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from Blog.models import Homestay,Blog,Author,Category
+
+
+admin.site.register(Homestay)
+admin.site.register(Blog)
+admin.site.register(Category)
+admin.site.register(Author)
+
+# Register your models here.
